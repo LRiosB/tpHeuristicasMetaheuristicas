05b40997e0eb42daa0df3b41842c1268ffeee7c1
LRiosB <lucas.riosb@gmail.com>
2025-01-21 17:15:31 -0300

    timestamps

--- experiments.py
+++ experiments.py
@@ -1,5 +1,7 @@
 from instances import getOptimizedInstance
 
+from time import time
+
 from strategyNearestNeighbor import nearestNeighbor
 from strategyNearestNeighborTeleports import nearestNeighborTeleports
 from strategyGreedyClustering import greedyClusteringOPTICS
@@ -22,25 +24,39 @@ if __name__ == "__main__":
 
         for percentage in [0.1, 0.25, 0.5, 0.75, 1]:
 
+            
+
             print(f"Object: {object}, Percentage: {percentage}")
 
             listObjects, listTeleports = getOptimizedInstance([object])
             numObjects = floor(percentage*len(listObjects))
 
+            t1 = time()
             nne = nearestNeighbor(listObjects, numObjects)
+            t2 = time()
             nnt = nearestNeighborTeleports(listObjects, listTeleports, numObjects)
+            t3 = time()
             gcl = greedyClusteringOPTICS(listObjects, listTeleports, numObjects)
+            t4 = time()
 
             nnevnd = SolutionWithVND(nne[0], listObjects, listTeleports)
             nnevnd.vnd()
+            t5 = time()
             nntvnd = SolutionWithVND(nnt[0], listObjects, listTeleports)
             nntvnd.vnd()
+            t6 = time()
             gclvnd = SolutionWithVND(gcl[0], listObjects, listTeleports)
             gclvnd.vnd()
+            t7 = time()
 
             newRow = {"Object": object, "Percentage": percentage, "Num objects": numObjects, "Total num objects": len(listObjects),
-                      "NNE": nne[2], "NNT": nnt[2], "GCL": gcl[2], 
-                      "NNE VND": nnevnd.totalDistanceWalked, "NNT VND": nntvnd.totalDistanceWalked, "GCL VND": gclvnd.totalDistanceWalked}
+                      "NNE": nne[2], "Duration NNE": t2-t1,
+                      "NNT": nnt[2], "Duration NNT": t3-t2,
+                      "GCL": gcl[2], "Duration GCL": t4-t3,
+                      "NNE VND": nnevnd.totalDistanceWalked, "Duration NNE VND": t5-t4,
+                      "NNT VND": nntvnd.totalDistanceWalked, "Duration NNT VND": t6-t5,
+                      "GCL VND": gclvnd.totalDistanceWalked, "Duration GCL VND": t7-t6,
+                      }
             newRow = pd.DataFrame([newRow])
 
             if df is None:
@@ -48,4 +64,4 @@ if __name__ == "__main__":
             else:
                 df = pd.concat([df, newRow], ignore_index=True)
 
-    df.to_csv("aux.csv", index=False)
+    df.to_csv("auxWithRuntimes.csv", index=False)
